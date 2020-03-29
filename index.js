252b0ff6d426a7c82c36a06fd5e8ba527785860f
Walter Scarano <walterscarano@outlook.com>
2020-03-29 12:43:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const app = require('express')();
 const fileUpload = require('express-fileupload');
 
-const app = express();
 app.use(fileUpload());
 
 app.post('/image', (req, res) => {
   if (!req.files) {
-    return res.status(400).send('');
+    return res.status(400).send({error: 'No file received'});
   }
-  //res.status(200).send(req.files.data.data.toString('base64'));
   res.status(200).json({ base64: req.files.data.data.toString('base64') });
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server inited');
+  console.log('Server running');
 });
